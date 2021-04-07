0d310715dc80994bcfd2ff2cd8fedc223d348a11
Anthony <anthony.zamarro@gmail.com>
2021-04-06 22:08:19 -0400

    start work for type ahead dropdown

--- src/components/SearchForSong.js
+++ src/components/SearchForSong.js
@@ -8,6 +8,7 @@ export default class SearchForSong extends React.Component {
         }
 
         this.onKeyUpHandler = this.onKeyUpHandler.bind(this);
+        this.onFocusHandler = this.onFocusHandler.bind(this);
     }
 
     onKeyUpHandler(event) {
@@ -26,15 +27,36 @@ export default class SearchForSong extends React.Component {
             }
         }
     }
+
+    onFocusHandler(event) {
+        if(event.currentTarget == event.target) {
+            console.log('focused', event);
+            event.target.insertAdjacentHTML('afterend', '<div>Hi</div>');
+        } else {
+            console.log('not focused', event);
+        }
+
+    }
     
     render() {
         return (
             <div>
                 <h2>Search for Song</h2>
-                <input type="text" onKeyUp={this.onKeyUpHandler} />
+                <input type="text" onKeyUp={this.onKeyUpHandler} onFocus={this.onFocusHandler}/>
                 <input type="submit" value="Search" />
             </div>
         )
     }
 
 }
+
+/*
+    when user focuses on input box, list of divs with song name appears
+    as user types, the list is filtered by each character typed
+    user can select one song from the list and that will fetch the marked versions
+
+    element listens to when it is focused (active?)
+    when focused is true, all songs display below input box (in array?) with max height and scrollable
+    while still focused, as user types, list is filtered by characters. use regex and filter function
+    user can click on song from list and that will be fetched for its marked version
+*/
