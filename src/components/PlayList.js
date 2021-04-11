161f2410b7dd21554f8afaa7c9741a5eeb6d76ad
Anthony <anthony.zamarro@gmail.com>
2021-04-11 14:38:05 -0400

    start adding drop handler

--- src/components/PlayList.js
+++ src/components/PlayList.js
@@ -11,10 +11,16 @@ export default class PlayList extends React.Component {
     }
 
     onDropHandler(e) {
-        console.log('onDropHandler', e);
+        console.log('onDropHandler', e)
+        e.preventDefault();
+        const data = e.dataTransfer.getData("text/plain");
+        console.log(data);
+//        e.target.appendChild(document.getElementById(data));;
     }
 
     onDragOverHandler(e) {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = 'move';
         //console.log('onDragOverHandler', e);
 
     }
@@ -24,7 +30,10 @@ export default class PlayList extends React.Component {
             <div className="playlist">
                 <h2>Play List</h2>
                 <ul onDrop={this.onDropHandler} 
-                    onDragOver={this.onDragOverHandler}>
+                    onDragOver={this.onDragOverHandler}
+                    id="target"
+                >
+
                     <li></li>
                 </ul>
             </div>
