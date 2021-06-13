5ad7fb68e736667bdc8d291d21281cea551b3e7f
Anthony <anthony.zamarro@gmail.com>
2021-06-13 11:28:27 -0400

    add h2 to song info

--- src/components/SongInfo.js
+++ src/components/SongInfo.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function SongInfo(song) {
     return (
         <div className="current-song__container container">
-            Song Info
+            <h2>Song Info</h2>
             <div>{song.currentSong && song.currentSong.title}</div>
             <div>{song.currentSong && song.currentSong.date}</div>
             <div>{song.currentSong && song.currentSong.venueName}</div>
